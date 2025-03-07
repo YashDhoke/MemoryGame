457578f2b68641353d8a8db7085b9416c1fc149f
Yash Dhoke <yashdhoke@Yashs-MacBook-Air.local>
2025-03-07 16:38:20 +0530

    adding the flipping logic

--- memoryGame/src/components/Grid.jsx
+++ memoryGame/src/components/Grid.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
+import { FaQuestion } from "react-icons/fa";
 
 const Grid = () => {
   const initialCards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
@@ -14,15 +15,26 @@ const Grid = () => {
 
   const [cards, setCards] = useState([]);
 
+  const [flipped, setFlipped] = useState([]);
+
   useEffect(() => {
     setCards(shuffleArray(initialCards));
   }, []);
 
+  const handleClick = (index) => {
+    if (flipped.includes(index)) return;
+    if (flipped.length === 2) return;
+
+    setFlipped([...flipped, index]);
+  };
+
+  console.log(flipped);
+
   return (
     <div className="grid">
       {cards.map((item, index) => (
-        <div className="box" key={index}>
-          {item}
+        <div className="box" key={index} onClick={() => handleClick(index)}>
+          {flipped.includes(index) ? item : <FaQuestion />}
         </div>
       ))}
     </div>
