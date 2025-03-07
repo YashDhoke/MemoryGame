4212c5002967d89d7b5ce989be3d8192cec9b0dc
Yash Dhoke <yashdhoke@Yashs-MacBook-Air.local>
2025-03-07 15:44:51 +0530

    added shuffling logic

--- memoryGame/src/components/Grid.jsx
+++ memoryGame/src/components/Grid.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 const Grid = () => {
+  const initialCards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    setCards(shuffleArray(initialCards));
+  }, []);
+
   return (
     <div className="grid">
-      {Array(16)
-        .fill(null)
-        .map((_, index) => (
-          <div className="box">{index + 1}</div>
-        ))}
+      {cards.map((item, index) => (
+        <div className="box" key={index}>
+          {item}
+        </div>
+      ))}
     </div>
   );
 };
