34371ef49b96695e9725fe66f4abf83c8a41c942
YashDhoke <yashdhoke1@gmail.com>
2025-03-10 20:10:45 +0530

    g-2

--- memoryGame/src/App.jsx
+++ memoryGame/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Grid from "./components/Grid";
 import "./App.css";
+import TicTacToe from "./components/TicTacToe";
 
 const App = () => {
   return (
     <div className="container">
-      <h1 className="heading">Memory Game</h1>
-      <Grid />
+      {/* <h1 className="heading">Memory Game</h1> */}
+      <h1 className="heading">Tic Tac Toe</h1>
+      {/* <Grid /> */}
+      <TicTacToe />
     </div>
   );
 };
---
+++ memoryGame/src/components/TicTacToe.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import { ImCross } from "react-icons/im";
+import { FaRegCircle } from "react-icons/fa";
+
+const TicTacToe = () => {
+  const [game, setGame] = useState(Array(9).fill(""));
+  const [turn, setTurn] = useState(1);
+
+  const handleClick = (index) => {
+    const newArr = [...game];
+    if (newArr[index] === "") {
+      newArr[index] = turn;
+      setTurn((prev) => (prev === 1 ? 0 : 1));
+      setGame(newArr);
+    }
+  };
+
+  useEffect(() => {
+    calculateResult();
+  }, [game]);
+
+  const calculateResult = () => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (game[a] !== "" && game[a] === game[b] && game[a] === game[c]) {
+        console.log(`Player ${game[a] === 1 ? "1 (X)" : "0 (O)"} Wins`);
+        return;
+      }
+    }
+  };
+
+  console.log(game);
+
+  return (
+    <div className="grid-2">
+      {game.map((item, index) => (
+        <div className="box-2" key={index} onClick={() => handleClick(index)}>
+          {item === 1 ? (
+            <ImCross className="icon-3" />
+          ) : item === 0 ? (
+            <FaRegCircle className="icon-2" />
+          ) : null}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TicTacToe;
